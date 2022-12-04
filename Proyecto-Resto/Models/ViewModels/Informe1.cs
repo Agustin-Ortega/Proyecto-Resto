2d7d62c8ac499091a12b82a20bdc1fc99c691e01
Agustin-Ortega <lpk900@gmail.com>
2022-12-04 01:30:37 -0300

    agrego viewModel de ganancias

---
+++ Proyecto-Resto/Models/ViewModels/Informe1.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Resto.Models.ViewModels
+{
+    public class Informe1
+    {
+
+        public DateTime Fecha { get; set; }
+
+        [Display(Name ="Nombre y apellido")]
+        public string Cliente { get; set; }
+
+        public List<string> ListaPlatos { get; set; }
+
+        public double Total { get; set; }
+
+        public double Ganancia { get; set; }
+    }
+}
