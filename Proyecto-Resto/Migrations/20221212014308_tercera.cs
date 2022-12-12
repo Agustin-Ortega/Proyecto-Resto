3cc50b9dea0a5894cac5722e524eed381d354d50
Agustin-Ortega <lpk900@gmail.com>
2022-12-12 02:49:36 -0300

    mejorando generador de reservas

---
+++ Proyecto-Resto/Migrations/20221212014308_tercera.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_Resto.Migrations
+{
+    public partial class tercera : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isProcessed",
+                table: "Reservas",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isProcessed",
+                table: "Reservas");
+        }
+    }
+}
